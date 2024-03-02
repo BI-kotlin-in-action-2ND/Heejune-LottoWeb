26a9170dcf1a4c466f2614e1d80309021829b9f0
Heejun-0219 <heejun0219@gmail.com>
2024-03-03 00:42:29 +0900

    init

---
+++ src/test/kotlin/com/lottoweb/lotto/LottoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lottoweb.lotto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LottoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
